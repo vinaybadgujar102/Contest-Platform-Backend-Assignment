226752e68af75bd66df914484b5af876b169324e
Vinay Badgujar <vinaybadgujar8@gmail.com>
2026-01-18 08:58:28 +0530

    feat: implement create dsa problem route

--- backend/src/routers/contest.router.ts
+++ backend/src/routers/contest.router.ts
@@ -1,7 +1,16 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
 import { authMiddleware, verifyUserRole } from "../middlewares/auth.middleware";
 import { UserRole } from "../types/UserRoles.type";
 import { ContestController } from "../controllers/contest.controller";
+import { schemaValidator } from "../validators";
+import {
+  createDsaProblemSchema,
+  type CreateDsaProblemInput,
+} from "../validators/contest.validator";
+import { prisma } from "../db/prisma";
+import { NotFoundError } from "../utils/app.error";
+import { successResponse } from "../utils/response.utils";
+import { StatusCodes } from "http-status-codes";
 
 const contestRouter = Router();
 
@@ -16,6 +25,46 @@ contestRouter.get(
   "/:contestId",
   authMiddleware,
   ContestController.getContestById,
+);
+
+contestRouter.post(
+  "/:contestId/dsa",
+  authMiddleware,
+  verifyUserRole(UserRole.CREATOR),
+  schemaValidator(createDsaProblemSchema),
+  async (req: Request, res: Response) => {
+    const payload = req.body as CreateDsaProblemInput;
+    const contestId = Number(req.params.contestId);
+
+    const contest = await prisma.contest.findFirst({
+      where: {
+        id: contestId,
+      },
+    });
+    if (!contest) {
+      throw new NotFoundError("CONTEST_NOT_FOUND");
+    }
+    const newContest = await prisma.dsaProblem.create({
+      data: {
+        title: payload.title,
+        description: payload.description,
+        tags: JSON.parse(payload.tags as string),
+        points: payload.points,
+        timeLimit: payload.timeLimit,
+        memoryLimit: payload.memoryLimit,
+        testCases: JSON.parse(payload.testCases.toString()),
+        contestId,
+      },
+      select: {
+        id: true,
+        contestId: true,
+      },
+    });
+    return successResponse(res, StatusCodes.CREATED, {
+      id: newContest.id,
+      contestId: newContest.contestId,
+    });
+  },
 );
 
 export default contestRouter;
--- backend/src/validators/contest.validator.ts
+++ backend/src/validators/contest.validator.ts
@@ -8,3 +8,21 @@ export const createContestSchema = z.object({
 });
 
 export type CreateContestInput = z.infer<typeof createContestSchema>;
+
+export const createDsaProblemSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  tags: ["array", "hash-table"],
+  points: z.number(),
+  timeLimit: z.number(),
+  memoryLimit: z.number(),
+  testCases: z.array(
+    z.object({
+      input: z.string(),
+      expectedOutput: z.string(),
+      isHidden: z.boolean().default(false),
+    }),
+  ),
+});
+
+export type CreateDsaProblemInput = z.infer<typeof createDsaProblemSchema>;
